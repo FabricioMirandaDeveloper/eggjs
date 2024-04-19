7aef14b11d9a5512dd14e21585adf0aa89596f99
Fabricio <mirandaquezadafabriciojoel@gmail.com>
2024-04-19 15:24:46 -0500

    sigo con la teoria

--- store/product.js
+++ store/product.js
@@ -34,10 +34,12 @@ class Product {
 const prod1 = new Product()
 const prod2 = new Product("01567","Leche", 2.5)
 const prod3= new Product("01568", "Toallitas Humedas", 7, 20, "https://i.postimg.cc/Jn2C5W84/galaxy1.webp", true)
+console.log("--------------");
+console.log("--------------");
+console.log("--------------");
 console.log(prod1,prod2,prod3)
+console.log(prod2.title)
+console.log(prod3.onsale);
 const prod4 = new Product("01478","Cuates",1,40,"https://i.postimg.cc/Jn2C5W84/galaxy1.webp)", false, "Frito Lays")
-console.log(prod4);
-/* console.log(prod4.setSupplier("
-Doritos"))
-console.log(prod4._supplier)
- */
+prod4.setSupplier ="Alicorp"
+console.log(prod4.getSupplier)
