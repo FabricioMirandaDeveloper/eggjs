31659d6702671c9ace2190844028fd47f9a745f6
Fabricio <mirandaquezadafabriciojoel@gmail.com>
2024-04-22 15:41:52 -0500

    practicando arrays

--- store/product.js
+++ store/product.js
@@ -40,7 +40,7 @@ class Product {
 
 const prod1 = new Product()
 const prod2 = new Product("01567","Leche", 2.5)
-const prod3= new Product("01568", "Toallitas Humedas", 7, 20, "https://i.postimg.cc/Jn2C5W84/galaxy1.webp", true)
+const prod3 = new Product("01568", "Toallitas Humedas", 7, 20, "https://i.postimg.cc/Jn2C5W84/galaxy1.webp", true)
 console.log("--------------");
 console.log("--------------");
 console.log("--------------");
@@ -54,3 +54,23 @@ const prod5 = new Product("03636","Doritos",2,12,[], false, "Bombo SAC")
 console.log(prod5.stock);
 prod5.sellUnits(13)
 console.log(prod5.stock);
+
+const products = [prod1,prod2,prod3,prod4,prod5]
+console.log(products);
+console.log(products[1]);
+console.log(products[products.length-1]);
+
+const prod6 = new Product("09562","Chizito",1,12,[],true,"Chips") 
+const prod7 = new Product("07548","Chettos",2,15,[],false,"Riverse")
+
+products.unshift(prod6)
+console.log(products);
+
+products.push(prod7)
+console.log(products);
+
+products.shift()
+console.log(products);
+
+products.pop()
+console.log(products);
