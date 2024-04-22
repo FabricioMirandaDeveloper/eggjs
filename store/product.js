98c0d75e2c47d31c82be76f740ccb7c056a3a173
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2024-04-21 23:09:17 -0500

    trabajando con arrays

--- store/product.js
+++ store/product.js
@@ -1,6 +1,6 @@
 /* ESTO ES UN OBJETO
  */
-const produto1 = {
+/* const produto1 = {
     nombre: "Celular",
     precio: 800,
     stock: 2
@@ -9,7 +9,7 @@ console.log(produto1);
 produto1.id = "01563"
 produto1["foto"] = "https://i.postimg.cc/Jn2C5W84/galaxy1.webp"
 console.log(produto1);
-console.log(produto1.nombre);
+console.log(produto1.nombre) */;
 
 /* ESTO ES UNA CLASE
  */
@@ -41,9 +41,6 @@ class Product {
 const prod1 = new Product()
 const prod2 = new Product("01567","Leche", 2.5)
 const prod3= new Product("01568", "Toallitas Humedas", 7, 20, "https://i.postimg.cc/Jn2C5W84/galaxy1.webp", true)
-console.log("--------------");
-console.log("--------------");
-console.log("--------------");
 console.log(prod1,prod2,prod3)
 console.log(prod2.title)
 console.log(prod3.onsale);
@@ -54,3 +51,9 @@ const prod5 = new Product("03636","Doritos",2,12,[], false, "Bombo SAC")
 console.log(prod5.stock);
 prod5.sellUnits(13)
 console.log(prod5.stock);
+
+const products = [prod1,prod2,prod3,prod4]
+console.log(products);
+console.log(products[1]);
+console.log(products[products.length-1]);
+console.log(products[products.length-1].title);
